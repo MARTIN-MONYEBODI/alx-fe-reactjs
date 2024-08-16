ed971c47f8f2bb4b80b40907c6c4369a0776f35e
MARTIN MONYEBODI <martinmonyebodi@gmail.com>
2024-08-16 11:14:30 +0200

    modified

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -30,10 +30,9 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <div>
-            <h1>Hello everyone, I am learning React at ALX!</h1>
-            <p>This is a simple JSX component.</p>
-            <p>I am learning about JSX!</p>
+      <WelcomeMessage />
       </div>
+
     </>
   )
 }
