fe46e7d2e92c66182353e1a43a17def853cbfc38
MARTIN MONYEBODI <martinmonyebodi@gmail.com>
2024-08-25 16:15:47 +0200

    components

---
+++ my-company/src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#333', color: '#fff', padding: '1rem', textAlign: 'center' }}>
+      <p>&copy; 2023 My Company</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ my-company/src/Components/Navbar.jsx
@@ -0,0 +1,30 @@
+function Navbar() {
+    return (
+      <nav style={{ backgroundColor: '#333', color: '#fff', padding: '1rem' }}>
+        <ul style={{ listStyle: 'none', display: 'flex', justifyContent: 'space-between' }}>
+          <li>
+            <Link to="/" style={{ color: '#fff', textDecoration: 'none' }}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" style={{ color: '#fff', textDecoration: 'none' }}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/services" style={{ color: '#fff', textDecoration: 'none' }}>
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" style={{ color: '#fff', textDecoration: 'none' }}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+  
+  export default Navbar;
