cdb224e04dd3878bf790fb2ece6582d7b302257e
MARTIN MONYEBODI <martinmonyebodi@gmail.com>
2024-08-25 16:16:28 +0200

    Modification

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -2,6 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './Home';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
+import Footer from './Footer';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +37,17 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+     </BrowserRouter>
+
     </>
   )
 }
