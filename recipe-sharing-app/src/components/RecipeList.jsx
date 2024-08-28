04415e86a67aa3c419f1ed38372642d0d51ff630
MARTIN MONYEBODI <martinmonyebodi@gmail.com>
2024-08-29 01:22:16 +0200

    Modifications

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,20 +1,20 @@
-import { useRecipeStore } from './recipeStore';
 import React from 'react';
+import { useRecipeStore } from '../recipeStore';
+import { Link } from 'react-router-dom';
 
 const RecipeList = () => {
-    const filteredRecipes = useRecipeStore(state => state.filteredRecipes);
-    const filterRecipes = useRecipeStore(state => state.filterRecipes);
-  
-    React.useEffect(() => {
-      filterRecipes();
-    }, [filterRecipes]);
-    
+  const filteredRecipes = useRecipeStore(state => state.filteredRecipes);
+  const filterRecipes = useRecipeStore(state => state.filterRecipes);
+
+  React.useEffect(() => {
+    filterRecipes();
+  }, [filterRecipes]);
 
   return (
     <div>
-      {recipes.map(recipe => (
-        <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
+      {filteredRecipes.map(recipe => (
+        <div key={(link unavailable)}>
+          <h3><Link to={`/recipes/${(link unavailable)}`}>{recipe.title}</Link></h3>
           <p>{recipe.description}</p>
         </div>
       ))}
