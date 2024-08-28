7f9903e0c7770826e92f9e01794545b5d491cf07
MARTIN MONYEBODI <martinmonyebodi@gmail.com>
2024-08-29 01:17:33 +0200

    Modified the App.jsx file to include the search bar and display filtered results

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import RecipeList from './components/RecipeList';
+import SearchBar from './components/SearchBar';
 import AddRecipeForm from './components/AddRecipeForm';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RecipeDetails from './components/RecipeDetails';
@@ -12,6 +13,10 @@ function App() {
 
   return (
     <BrowserRouter>
+       <div>
+        <SearchBar />
+        <RecipeList />
+      </div>
       <Routes>
         <Route path="/" element={<RecipeList />} />
         <Route path="/recipes/:id" element={<RecipeDetails />} />
