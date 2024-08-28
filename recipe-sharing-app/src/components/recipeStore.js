68a7f78082ed41a61ed5539b196b3f2184146f7c
MARTIN MONYEBODI <martinmonyebodi@gmail.com>
2024-08-29 01:08:18 +0200

    Modiied  the recipeStore.js file to include search and filtering capabilities

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,10 +2,12 @@ import create from 'zustand'
 
 const useRecipeStore = create(set => ({
   recipes: [],
-  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  deleteRecipe: (id) => set(state => ({ recipes: state.recipes.filter(recipe => (link unavailable) !== id) })),
-  updateRecipe: (updatedRecipe) => set(state => ({ recipes: state.recipes.map(recipe => (link unavailable) === (link unavailable) ? updatedRecipe : recipe) })),
-  setRecipes: (recipes) => set({ recipes })
+  searchTerm: '',
+  setSearchTerm: (term) => set({ searchTerm: term }),
+  filteredRecipes: [],
+  filterRecipes: () => set(state => ({
+    filteredRecipes: state.recipes.filter(recipe => recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase()))
+  })),
 }));
 
 export default useRecipeStore;
