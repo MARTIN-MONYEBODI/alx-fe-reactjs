27eca9a41ec6d17545c9412a6a4c7c701caaa41e
MARTIN MONYEBODI <martinmonyebodi@gmail.com>
2024-08-29 00:40:28 +0200

    Modified  the recipeStore.js file to include actions for updating and deleting recipes

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,6 +3,8 @@ import create from 'zustand'
 const useRecipeStore = create(set => ({
   recipes: [],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+  deleteRecipe: (id) => set(state => ({ recipes: state.recipes.filter(recipe => (link unavailable) !== id) })),
+  updateRecipe: (updatedRecipe) => set(state => ({ recipes: state.recipes.map(recipe => (link unavailable) === (link unavailable) ? updatedRecipe : recipe) })),
   setRecipes: (recipes) => set({ recipes })
 }));
 
