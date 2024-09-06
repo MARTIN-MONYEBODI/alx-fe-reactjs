a681c9d2d76b677beea30363a59a72a0c2028c6f
MARTIN MONYEBODI <martinmonyebodi@gmail.com>
2024-09-06 12:29:40 +0200

    file updated to use FormikForm component

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import RegistrationForm from './components/RegistrationForm';
+import FormikForm from './components/FormikForm';
 
 function App() {
   return (
     <div className="App">
-      <h1>Registration Form</h1>
-      <RegistrationForm />
+      <h1>Registration Form with <Formik></Formik></h1>
+      <FormikForm />
     </div>
   );
 }
