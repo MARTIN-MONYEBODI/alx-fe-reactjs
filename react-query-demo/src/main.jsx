29a845c52d31f36177323f1627df89427a31de0f
MARTIN MONYEBODI <martinmonyebodi@gmail.com>
2024-09-08 23:46:24 +0200

    Update src/main.jsx to include React Query Devtools

--- react-query-demo/src/main.jsx
+++ react-query-demo/src/main.jsx
@@ -1,13 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import App from './App';
-
-const queryClient = new QueryClient();
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 ReactDOM.render(
   <QueryClientProvider client={queryClient}>
     <App />
+    <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>,
   document.getElementById('root')
 );
