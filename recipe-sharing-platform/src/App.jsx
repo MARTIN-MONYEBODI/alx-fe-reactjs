9ed29f7410cb252e35bba84efd3ad5d31cfc4047
MARTIN MONYEBODI <martinmonyebodi@gmail.com>
2024-09-16 22:18:53 +0200

    Modifications

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-blue-500">Hello Tailwind CSS!</h1>
+      <HomePage />
     </div>
   );
 }
