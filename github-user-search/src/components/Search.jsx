c56ee5711542f574059e9e135b8960080eef7972
MARTIN MONYEBODI <martinmonyebodi@gmail.com>
2024-10-02 00:16:16 +0200

    Modifications

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,4 +1,4 @@
-iimport React, { useState } from 'react';
+import React, { useState } from 'react';
 import { fetchAdvancedSearchResults } from '../services/githubService';
 
 const Search = () => {
